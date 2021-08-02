a8d94ec0fcd7b6bc7b6da47311e7f6f93bbe4bc1
cnjm <1837847333@qq.com>
2021-08-02 18:02:19 +0800

    blog

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,8 @@ const themeConfig = require('./config/themeConfig.js');
 module.exports = {
   title: 'CNJM | DOCUMENT',
   description: '即便是风，吹过也留下一片清凉',
+  base: "/blog/",
+  dest: "blog",
   head,
   themeConfig
 }
